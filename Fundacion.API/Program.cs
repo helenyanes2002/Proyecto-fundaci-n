086fc67cbc03b370e64be20045ae85b1d65163f3
MARYORI MONTOYA CORRALES <maryorimontoya1113908@gmail.com>
2024-05-15 22:46:45 -0500

    Se agrega vista de donaciones

--- Fundacion.API/Program.cs
+++ Fundacion.API/Program.cs
@@ -6,31 +6,43 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<DataContext>(x=>x.UseSqlServer("name=DefaultConnection"));
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+SeedData(app);
+
+static void SeedData(WebApplication app)
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.MapControllers();
 
-app.UseCors(x => x
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory!.CreateScope())
+    {
+
+    }
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.UseCors(x => x
 
     .AllowAnyMethod()
-
     .AllowAnyHeader()
-
+    .AllowCredentials()
     .SetIsOriginAllowed(origin => true)
-
-    .AllowCredentials());
-
-app.Run();
+    );
+    app.Run();
+}
