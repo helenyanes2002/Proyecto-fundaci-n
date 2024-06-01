3d281d586df7b27b4993e0701d9cfd7aabdc0335
helenyanes2002 <helenyanes8@gmail.com>
2024-06-01 18:18:13 -0500

    Terminando parte del archivo SeedDB

--- Fundacion.API/Program.cs
+++ Fundacion.API/Program.cs
@@ -1,5 +1,9 @@
 using Fundacion.API.Data;
+using Fundacion.API.Helpers;
+using Fundacion.Shared.Entidades;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,22 +12,48 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+
+
+
+
+builder.Services.AddIdentity<User, IdentityRole>(x =>
+{
+    
+    x.SignIn.RequireConfirmedEmail = true;
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequireDigit = false;
+    x.Password.RequiredUniqueChars = 0;
+    x.Password.RequireLowercase = false;
+    x.Password.RequireNonAlphanumeric = false;
+
+})
+
+
+.AddEntityFrameworkStores<DataContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IUserHelper, UserHelper>();
+builder.Services.AddTransient < SeedDb > ();
+
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(x=>x.UseSqlServer("name=DefaultConnection"));
+ builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
+
+
 var app = builder.Build();
 
 SeedData(app);
 
 static void SeedData(WebApplication app)
 {
-
-
     var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
 
     using (var scope = scopedFactory!.CreateScope())
     {
-
+        var service = scope.ServiceProvider.GetService<SeedDb>();
+        service!.SeedAsync().Wait();
     }
+
+
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
--- Fundacion.Shared/Entidades/User.cs
+++ Fundacion.Shared/Entidades/User.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Fundacion.Shared.Enums;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -11,28 +12,28 @@ namespace Fundacion.Shared.Entidades
     public class User: IdentityUser
     {
 
-        public int Id { get; set; }
-
-        [Display(Name = "Nombre")]
-        [MaxLength(50, ErrorMessage = "No se permiten más de 50 caracteres")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        public string Nombre { get; set; }
-
-        [Display(Name = "Dirección")]
-        [MaxLength(50, ErrorMessage = "No se permiten más de 50 caracteres")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        public string Direccion { get; set; }
-
-        [Display(Name = "Correo")]
-        [MaxLength(50, ErrorMessage = "No se permiten más de 50 caracteres")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        [EmailAddress(ErrorMessage = "Digite un correo válido")]
-        public string Correo { get; set; }
-
-        [Display(Name = "Teléfono")]
         [MaxLength(20, ErrorMessage = "No se permiten más de 20 dígitos")]
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        public string Telefono { get; set; }
+        public string Document { get; set; }
+
+
+        [MaxLength(50, ErrorMessage = "No se permiten más de 50 Caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string FirstName { get; set; }
+
+
+        [MaxLength(50, ErrorMessage = "No se permiten más de 50 Caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string LastName { get; set; }
+
+
+        public string Address { get; set; }
+
+        [Display(Name = "Foto")]
+        public string Photo { get; set; }
+        public UserType UserType { get; set; }
+
+        public string FullName => $"{FirstName}{LastName}";
 
 
 
